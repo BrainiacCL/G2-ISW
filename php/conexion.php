124684036734bd6f67e5834d9099241890c90d76
Brayan Isaac Elgueta Antiman <brayan.elgueta1501@alumnos.ubiobio.cl>
2020-03-24 22:58:35 -0300

    Se agrega carpeta presupusto

--- php/conexion.php
+++ php/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost","root", "","clinicadental");
+$mysqli = new mysqli("localhost","root", "","clinicadental_sql");
 if ($mysqli->connect_errno){
     echo "Fallo al conectar con la BD: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
---
+++ php/presupuesto/conexion.php
@@ -0,0 +1,7 @@
+<?php
+
+$conectar = mysqli_connect("localhost", "root", "", "clinicadental_sql") or die ("Error");
+
+// $conectar = mysqli_connect("localhost","root","mysql","siaprueba") or die ("Error");
+
+?>
